1737113db0f3df06ff760cc18ced766785148882
tomo6300 <tomoyagishita@icloud.com>
2021-10-29 01:10:43 +0900

    add fix

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
@@ -3,7 +3,6 @@
  */
 package jp.co.yumemi.android.code_check
 
-import android.content.Context
 import android.os.Parcelable
 import androidx.lifecycle.ViewModel
 import io.ktor.client.*
@@ -12,6 +11,7 @@ import io.ktor.client.engine.android.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import jp.co.yumemi.android.code_check.TopActivity.Companion.lastSearchDate
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
@@ -22,16 +22,15 @@ import java.util.*
 /**
  * TwoFragment で使う
  */
-class OneViewModel(
-    val context: Context
-) : ViewModel() {
+class OneViewModel : ViewModel() {
 
     // 検索結果
-    fun searchResults(inputText: String): List<item> = runBlocking {
+    @DelicateCoroutinesApi
+    fun searchResults(inputText: String): List<Item> = runBlocking {
         val client = HttpClient(Android)
 
         return@runBlocking GlobalScope.async {
-            val response: HttpResponse = client?.get("https://api.github.com/search/repositories") {
+            val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
                 header("Accept", "application/vnd.github.v3+json")
                 parameter("q", inputText)
             }
@@ -40,7 +39,7 @@ class OneViewModel(
 
             val jsonItems = jsonBody.optJSONArray("items")!!
 
-            val items = mutableListOf<item>()
+            val items = mutableListOf<Item>()
 
             /**
              * アイテムの個数分ループする
@@ -52,14 +51,14 @@ class OneViewModel(
                 val language = jsonItem.optString("language")
                 val stargazersCount = jsonItem.optLong("stargazers_count")
                 val watchersCount = jsonItem.optLong("watchers_count")
-                val forksCount = jsonItem.optLong("forks_conut")
+                val forksCount = jsonItem.optLong("forks_count")
                 val openIssuesCount = jsonItem.optLong("open_issues_count")
 
                 items.add(
-                    item(
+                    Item(
                         name = name,
                         ownerIconUrl = ownerIconUrl,
-                        language = context.getString(R.string.written_language, language),
+                        language = language,
                         stargazersCount = stargazersCount,
                         watchersCount = watchersCount,
                         forksCount = forksCount,
@@ -76,7 +75,8 @@ class OneViewModel(
 }
 
 @Parcelize
-data class item(
+//クラス名は大文字
+data class Item(
     val name: String,
     val ownerIconUrl: String,
     val language: String,
--- app/src/main/kotlin/jp/co/yumemi/android/code_check/TwoFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/TwoFragment.kt
@@ -26,14 +26,15 @@ class TwoFragment : Fragment(R.layout.fragment_two) {
 
         binding = FragmentTwoBinding.bind(view)
 
-        var item = args.item
+        val item = args.item
 
-        _binding.ownerIconView.load(item.ownerIconUrl);
-        _binding.nameView.text = item.name;
-        _binding.languageView.text = item.language;
-        _binding.starsView.text = "${item.stargazersCount} stars";
-        _binding.watchersView.text = "${item.watchersCount} watchers";
-        _binding.forksView.text = "${item.forksCount} forks";
-        _binding.openIssuesView.text = "${item.openIssuesCount} open issues";
+        _binding.ownerIconView.load(item.ownerIconUrl)
+        _binding.nameView.text = item.name
+        _binding.languageView.text =
+            context?.getString(R.string.written_language, item.language) ?: context?.getString(R.string.written_in_kotlin)
+        _binding.starsView.text = context?.getString(R.string._stars_count, item.stargazersCount)
+        _binding.watchersView.text = context?.getString(R.string._watchers_count, item.watchersCount)
+        _binding.forksView.text = context?.getString(R.string._forks_count, item.forksCount)
+        _binding.openIssuesView.text = context?.getString(R.string._open_issues_count, item.openIssuesCount)
     }
 }
